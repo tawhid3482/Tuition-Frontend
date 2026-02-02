e7374e16db07b08ebbad6bbbafbd1417704fc653
Tawhidul Islam <tawhidulislam3482@gmail.com>
2026-02-02 14:13:08 +0600

    fix

--- src/app/(withCommonLayout)/layout.tsx
+++ src/app/(withCommonLayout)/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/src/components/Shared/Navbar/page";
 const CommonLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <div className="w-7xl mx-auto ">
+      <div className="w-full md:w-7xl mx-auto ">
         <Navbar />
         <div className=" h-screen overflow-y-auto">{children}</div>
         <Footer />
