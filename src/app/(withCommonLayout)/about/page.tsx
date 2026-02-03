29329d86d9ab683646f1e81cd591803b92bd83ba
Tawhidul Islam <tawhidulislam3482@gmail.com>
2026-02-03 17:40:01 +0600

    Fix

--- src/app/(withCommonLayout)/about/page.tsx
+++ src/app/(withCommonLayout)/about/page.tsx
@@ -1,43 +1,11 @@
-'use client'
-import { useGetAllUserQuery } from '@/src/redux/features/user/userApi';
+import React from 'react';
 
-const Page = () => {
-const {data:users}=useGetAllUserQuery(undefined)
-
-// const users = user?.data
-
-console.log("All Users:", users);   
-
-//   useEffect(() => {
-//     // Fetch all users from your API
-//     fetch('http://localhost:5000/api/v1/user/allUsers')
-//       .then((res) => {
-//         if (!res.ok) {
-//           throw new Error('Network response was not ok');
-//         }
-//         return res.json();
-//       })
-//       .then((data) => {
-//         setUsers(data);
-//         setLoading(false);
-//       })
-//       .catch((err) => {
-//         setError(err.message);
-//         setLoading(false);
-//       });
-//   }, []);
-
-
+const page = () => {
   return (
     <div>
-      <h1>All Users</h1>
-      {/* <ul>
-        {users.map((user:any) => (
-          <li key={user._id}>{user.name}</li>
-        ))}
-      </ul> */}
+      
     </div>
   );
 };
 
-export default Page;
+export default page;
