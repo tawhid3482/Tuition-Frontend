67434f28fcaa5b4feaf90f498647cbb24c27a126
Tawhidul Islam <tawhidulislam3482@gmail.com>
2026-02-07 16:06:57 +0600

    Fix

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,30 +1,41 @@
 "use client";
 
-
+import { useState } from "react";
 import Lottie from "lottie-react";
-import img from '../../app/assets/Teacher.json';
+import img from "../../app/assets/Teacher.json";
+import BannerSkeleton from "./BannerSkeletion";
 
 const BannerPage = () => {
-    return (
-        <div>
-            <div className="flex items-center justify-center">
-                {/* left column  */}
-                <div className="border md:px-10 md:py-3.5 sm:space-y-1.5 space-y-2.5 md:space-y-3.5">
-                    <h2 className="sm:text-xl text-3xl md:text-4xl font-medium md:font-bold">Best <span className="text-primary">Tutoring Platform</span> <br />
-                        for Home & Online Tuitions</h2>
-                    <h3 className="text-3xl">Find the Right Tutor in Your Area</h3>
-                </div>
+  const [isLottieLoaded, setIsLottieLoaded] = useState(false);
 
-                {/* right column  */}
-                <div>
-                    <div className="bg-primary bg-clip-text text-transparent">
-                        <Lottie className="w-full" animationData={img}>
-                        </Lottie>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="min-h-87.5 flex items-center justify-center">
+      <div className="flex items-center justify-center gap-6">
+        {/* left column (always visible) */}
+        <div className="md:px-10 md:py-3.5 space-y-3.5">
+          <h2 className="text-3xl md:text-4xl font-bold">
+            Best <span className="text-primary">Tutoring Platform</span> <br />
+            for Home & Online Tuitions
+          </h2>
+          <h3 className="text-xl md:text-2xl">
+            Find the Right Tutor in Your Area
+          </h3>
         </div>
-    );
+
+        {/* right column */}
+        <div className="w-87.5">
+          {!isLottieLoaded && <BannerSkeleton />}
+
+          <Lottie
+            animationData={img}
+            loop
+            onDOMLoaded={() => setIsLottieLoaded(true)}
+            className={isLottieLoaded ? "block" : "hidden"}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BannerPage;
---
+++ src/components/Banner/BannerSkeletion.tsx
@@ -0,0 +1,10 @@
+const BannerSkeleton = () => {
+  return (
+    <div className="flex items-center justify-center animate-pulse">     
+      {/* right skeleton */}
+      <div className="w-87.5 h-75 bg-gray-200 rounded-xl ml-6"></div>
+    </div>
+  );
+};
+
+export default BannerSkeleton;
