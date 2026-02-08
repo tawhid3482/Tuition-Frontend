3181edd4b93eb2a2ee91acd91514a4acd0914baa
Tawhidul Islam <tawhidulislam3482@gmail.com>
2026-02-08 16:52:12 +0600

    Fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,76 +1,7 @@
-// import type { Metadata } from "next";
-// import "./globals.css";
-// import { Geist, Geist_Mono, Instrument_Serif } from "next/font/google";
-// import { Toaster } from "react-hot-toast";
-// import Providers from "@/src/lib/Providers/Providers";
-// import { getSettings } from "@/src/lib/api/getSettings";
-// import AuthProvider from "../lib/Providers/AuthProvider";
-// import { useFCM } from "../hooks/useFCM";
-
-// const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-// const instrumentSerif = Instrument_Serif({
-//   variable: "--font-instrument-serif",
-//   subsets: ["latin"],
-//   weight: ["400"],
-// });
-
-// export const metadata: Metadata = {
-//   title: "Code Base",
-//   description: "Modern SEO friendly web application",
-//   robots: "index, follow",
-// };
-
-// export default async function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   const settings = await getSettings(); // ✅ SERVER SIDE
-//     useFCM();
-
-
-//   return (
-//     <html lang="en">
-//       <body
-//         style={
-//           {
-//             "--primary": settings.primaryColor,
-//             "--secondary": settings.secondaryColor,
-//             "--accent": settings.accentColor,
-//             "--text": settings.textColor,
-//             "--text-secondary": settings.textSecondary,
-//             "--border": settings.borderColor,
-//             "--active": settings.btnActive,
-//             "--card-bg": settings.cardBg,
-//             "--btn-bg": settings.btnBg,
-//             "--btn-text": settings.btnText,
-//             "--btn-hover": settings.btnHover,
-//             "--btn-active": settings.btnActive,
-//           } as React.CSSProperties
-//         }
-//         className={`${geistSans.variable} ${geistMono.variable} ${instrumentSerif.variable} antialiased`}
-//       >
-//         <Providers>
-//           <AuthProvider>
-//             <Toaster position="top-right" />
-//             {children}
-//           </AuthProvider>
-//         </Providers>
-//       </body>
-//     </html>
-//   );
-// }
-
-
-
 // src/app/layout.tsx
 import type { Metadata } from "next";
 import "./globals.css";
-import { Geist, Geist_Mono, Instrument_Serif } from "next/font/google";
+import { Geist_Mono } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import Providers from "@/src/lib/Providers/Providers";
 import { getSettings } from "@/src/lib/api/getSettings";
@@ -84,13 +15,9 @@ const openSans = Open_Sans({
   display: "swap",
 });
 
-
-const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
-const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
-const instrumentSerif = Instrument_Serif({
-  variable: "--font-instrument-serif",
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
   subsets: ["latin"],
-  weight: ["400"],
 });
 
 export const metadata: Metadata = {
@@ -111,6 +38,8 @@ export default async function RootLayout({
       <body
         style={
           {
+            backgroundColor: "white",
+            color: "black",
             "--primary": settings.primaryColor,
             "--secondary": settings.secondaryColor,
             "--accent": settings.accentColor,
